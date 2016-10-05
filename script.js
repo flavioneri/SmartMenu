e87350fb48539dc38f589725815e95abb83780a3
Joana Araujo <joana22araujo@gmail.com>
2016-10-05 22:43:30 +0100

    Identifiquei a posicao do array

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ var app = angular.module("menuApp", []);
 
 app.controller('menuController', ["$scope", function($scope,$http, $location,filter){
   $scope.data = [
-    {id_prato: 1, nome: "Bacalhau à Brás", preco: "10.00€"},
-    {id_prato: 20, nome: "Picanha", preco: "15.00€"},
-    {id_prato: 3, nome: "Arroz de Marisco", preco: "18.50€"},
-    {id_prato: 4, nome: "Francesinha", preco: "12.5€"},
-    {id_prato: 5, nome: "Arroz de Pato", preco: "9.00€"}
+    {ref_prato: 1, nome: "Bacalhau à Brás", preco: "10.00€"}, //id = 0 no array
+    {ref_prato: 3, nome: "Arroz de Marisco", preco: "18.50€"},
+    {ref_prato: 4, nome: "Francesinha", preco: "12.5€"},
+    {ref_prato: 5, nome: "Arroz de Pato", preco: "9.00€"},
+    {ref_prato: 20, nome: "Picanha", preco: "15.00€"}
+
   ];
   
 $scope.currentId = -1; //Id da receita do botao que foi clicado para quando clicamos no comenatario saber qual foi o prato
-$scope.ler=function(id_prato){
-	$scope.currentId = id_prato;
+$scope.ler=function(ref_prato_click){
+	$scope.currentId = ref_prato_click;
+	
+	// identificar linha do array com o ref_prato correspondente
+	var posicaoArray = 0;
+	for(var i=0; i<$scope.data.length;i++){
+		if($scope.data[i].ref_prato == ref_prato_click){
+			posicaoArray = i;
+	
+		}
+	}
 
-	//var guarda_nomePrato = document.getElementById("modal_div1");
-
-
+	//Mudar os conteudos do modal
+	document.getElementById('modal_title').innerHTML= $scope.data[posicaoArray].nome;
+		
 }
 
 $scope.comment=function(){
