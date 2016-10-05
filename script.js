d093f70d5ca8f8071eb4d6001d04049b5209b598
Joana Araujo <joana22araujo@gmail.com>
2016-10-05 21:34:18 +0100

    Adicionei o modal para apresentar os pratos

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 var app = angular.module("menuApp", []);
 
-app.controller("menuController", ["$scope","$window", function($scope,$window){
+app.controller('menuController', ["$scope", function($scope,$http, $location,filter){
   $scope.data = [
-    {nome: "Bacalhau à Brás", preco: "10€"},
-    {nome: "Picanha", preco: "15€"},
-    {nome: "Arroz de Marisco", preco: "18.50€"}
-    ];
+    {id_prato: 1, nome: "Bacalhau à Brás", preco: "10.00€"},
+    {id_prato: 20, nome: "Picanha", preco: "15.00€"},
+    {id_prato: 3, nome: "Arroz de Marisco", preco: "18.50€"},
+    {id_prato: 4, nome: "Francesinha", preco: "12.5€"},
+    {id_prato: 5, nome: "Arroz de Pato", preco: "9.00€"}
+  ];
+  
+$scope.currentId = -1; //Id da receita do botao que foi clicado para quando clicamos no comenatario saber qual foi o prato
+$scope.ler=function(id_prato){
+	$scope.currentId = id_prato;
+
+	//var guarda_nomePrato = document.getElementById("modal_div1");
 
 
+}
 
+$scope.comment=function(){
+	if($scope.currentId>0){
+		alert("Comentario no prato "+$scope.currentId)
+	}
+}
 
 
 }]);
-
-
-//alert($scope.data[0])
-
-//alert(document.getElementById("myTable").rows[1].innerHTML);
-
-//alert("Row index is: " + nome.rowIndex[]);
